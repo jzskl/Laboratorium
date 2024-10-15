64556ad96014295ad757235e6274083233b21065
jzskl <184993527+jzskl@users.noreply.github.com>
2024-10-15 16:25:28 +0200

    add birth

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class BirthController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    public IActionResult Result(BirthModel model)
+    {
+        if (!model.IsValid())
+        {
+            return View("CustomError");
+        }
+        return View(model);
+    }
+}
---
+++ WebApp/Models/BirthModel.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApp.Models;
+
+public class BirthModel
+{
+    public string? Name { get; set; }
+    public DateTime? Date { get; set; }
+
+    public bool IsValid()
+    {
+        return Name != null && Date != null && Date < DateTime.Now;
+    }
+
+    public int Birth()
+    {
+        DateTime birth = (DateTime)Date;
+        DateTime future = DateTime.Now;
+        
+        int years = future.Year - birth.Year;
+        if (birth.Month == future.Month && future.Day < birth.Day || future.Month < birth.Month)
+        {
+            years--;
+        }
+
+        return years;
+    }
+}
